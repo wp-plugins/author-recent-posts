3e9c0f4b1b3fba0f3fffb6b23ae12090640bb982
amitaits <amitaits@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-06 07:59:49 +0000

    initial version of author recent posts plugin
    git-svn-id: https://plugins.svn.wordpress.org/author-recent-posts/trunk@888236 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ author_recent_posts.php
@@ -0,0 +1,170 @@
+<?php
+/*
+Plugin Name: Author Recent Posts
+Plugin URI: http://dev.fellowtuts.com/author-recent-posts-plugin/
+Description: Author Recent Posts plugin shows recent posts by an author on his/her posts as a responsive sidebar widget
+Version: 1.0
+Author: Amit Sonkhiya, Kamal Agrawal
+Author URI: http://dev.fellowtuts.com
+License: GPLv2 or later
+*/
+
+ 
+ class authorRecentPosts extends WP_Widget {
+
+	// constructor
+	function authorRecentPosts() {
+		parent::__construct(
+		'author_recent_posts', // Base ID
+			'Author Recent Posts', // Name
+			array( 'description' => __( 'Display recent posts by an author on his/her posts as a responsive sidebar widget', 'text_domain' ), ) // Args
+		);
+	}
+	
+	
+	// widget form creation
+	function form($instance) {	
+	// Check values	
+	$title     = isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : '';
+	$numberofposts   = isset( $instance['numberofposts'] ) ? absint( $instance['numberofposts'] ) : 5;
+	$show_date = isset( $instance['show_date'] ) ? (bool) $instance['show_date'] : false;
+	$showthumbnail = isset( $instance['showthumbnail'] ) ? (bool) $instance['showthumbnail'] : false;
+	$width = isset( $instance['width'] ) ?  esc_attr($instance['width']) : '';
+	$height = isset( $instance['height'] ) ? esc_attr( $instance['height']) : '';
+	
+		?>
+	<p>
+	<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title', 'wp_widget_plugin'); ?></label>
+	<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+	</p>
+
+	<p>
+	<label for="<?php echo $this->get_field_id('numberofposts'); ?>"><?php _e('Number of posts to show:', 'wp_widget_plugin'); ?></label>
+	<input  id="<?php echo $this->get_field_id('numberofposts'); ?>" name="<?php echo $this->get_field_name('numberofposts'); ?>" type="text"  size="3" value="<?php echo $numberofposts; ?>" />
+	</p>
+    
+	<p>
+	
+	<input class="checkbox showthumbnail" id="<?php echo $this->get_field_id('showthumbnail'); ?>" name="<?php echo $this->get_field_name('showthumbnail'); ?>" type="checkbox" <?php checked( $showthumbnail ); ?> /><label for="<?php echo $this->get_field_id('showthumbnail'); ?>"><?php _e('Show Thumbnail?', 'wp_widget_plugin'); ?></label>
+    
+	</p>
+    <div class="thumbnailAttr">
+    <p>
+		<label for="<?php echo $this->get_field_id( 'width' ); ?>"><?php _e( 'Width: ' ); ?></label> 
+		<input  size="3" id="<?php echo $this->get_field_id( 'width' ); ?>" name="<?php echo $this->get_field_name( 'width' ); ?>" type="text" value="<?php echo $width; ?>" /> px
+		</p>
+        <p>
+		<label for="<?php echo $this->get_field_id( 'height' ); ?>"><?php _e( 'Height: ' ); ?></label> 
+		<input  size="3"  id="<?php echo $this->get_field_id( 'height' ); ?>" name="<?php echo $this->get_field_name( 'height' ); ?>" type="text" value="<?php echo $height; ?>" /> px
+		</p>
+        </div>
+    
+    <p><input class="checkbox" type="checkbox" <?php checked( $show_date ); ?> id="<?php echo $this->get_field_id( 'show_date' ); ?>" name="<?php echo $this->get_field_name( 'show_date' ); ?>" />
+		<label for="<?php echo $this->get_field_id( 'show_date' ); ?>"><?php _e( 'Display post date?' ); ?></label></p>
+     
+    
+<?php	}
+
+	// widget update
+	function update($new_instance, $old_instance) {
+		
+		$old_instance['title'] = $new_instance['title'];		
+		$old_instance['numberofposts'] = isset($new_instance['numberofposts'])?(int)$new_instance['numberofposts']:'';
+		
+		$old_instance['showthumbnail'] = isset( $new_instance['showthumbnail'] ) ? (bool) $new_instance['showthumbnail'] : false;
+		$old_instance['width'] = isset($new_instance['width'])?$new_instance['width']:'';
+		$old_instance['height'] = isset($new_instance['height'])?$new_instance['height']:'';
+		$old_instance['show_date'] = isset( $new_instance['show_date'] ) ? (bool) $new_instance['show_date'] : false;
+		
+		return $old_instance ;
+	
+	}
+	
+	
+	
+	// widget display
+	function widget($args, $instance) {
+			extract($args);
+
+		$title = ( ! empty( $instance['title'] ) ) ? $instance['title'] : __( 'Author Recent Posts' );
+		$title = apply_filters( 'widget_title', $title, $instance, $this->id_base );
+		$number = ( ! empty( $instance['numberofposts'] ) ) ? absint( $instance['numberofposts'] ) : 5;
+		$showthumbnail = isset( $instance['showthumbnail'] ) ? $instance['showthumbnail'] : false;
+		$width_image = empty($instance['width']) ? '50' : apply_filters('widget_image_width', $instance['width']);
+        $height_image = empty($instance['height']) ? '50' : apply_filters('widget_image_height', $instance['height']);
+		
+		$show_date = isset( $instance['show_date'] ) ? $instance['show_date'] : false;
+		
+	
+        if(is_single()){
+		 global $authordata, $post;
+         
+         $authors_posts = get_posts( array( 'author' => $authordata->ID, 
+		'post__not_in' => array( $post->ID ), 'posts_per_page' => $number ) );
+	if (count($authors_posts) > 0) :
+		?>
+		<?php echo $before_widget; ?>
+		<?php if ( $title ) 
+			{
+				$author = get_the_author_meta( 'display_name', $authordata->ID );
+				$title = str_replace('[author]', $author, $title);
+				echo $before_title . $title . $after_title; 
+			}
+		?>
+		<ul class="author_post">
+		<?php foreach ( $authors_posts as $authors_post ) { ?>
+			<li>
+            <?php if($showthumbnail) : ?>
+            <div class="author_left" style="width:<?php echo $width_image; ?>px;height:<?php echo $height_image; ?>px;">           
+            <?php
+			if( $showthumbnail && has_post_thumbnail( $authors_post->ID )){
+				
+				?>
+                 <a href="<?php echo get_permalink( $authors_post->ID ) ; ?>">
+                <?php
+				echo get_the_post_thumbnail($authors_post->ID, array($width_image,$height_image)); ?>
+                </a>
+			<?php }
+			 ?>
+            
+            </div>
+            <?php endif; ?>
+            <div class="author_right">
+            <a href="<?php echo get_permalink( $authors_post->ID ) ; ?>">
+            <?php			
+            echo apply_filters( 'the_title', $authors_post->post_title, $authors_post->ID ); ?>
+            </a>
+			<?php if ( $show_date ) : ?>
+				<br /><span class="post-date"><small><?php echo get_the_date(); ?></small></span>
+			<?php endif; ?>
+            </div>
+			</li>
+		<?php } ?>
+		</ul>
+		<?php echo $after_widget; ?>
+<?php
+		// Reset the global $the_post as this query will have stomped on it
+		wp_reset_postdata();		
+		endif ;
+		} // single page condition ends here
+	}// widget function end here
+
+	
+ } // class end tag
+// register widget
+function admin_author_recent_posts_js() {
+            wp_register_script('admin_author_script', plugins_url('js/authorrecentposts.js', __FILE__) );
+            wp_enqueue_script('admin_author_script');
+        }
+add_action ('admin_enqueue_scripts','admin_author_recent_posts_js');
+add_action('widgets_init', create_function('', 'return register_widget("authorRecentPosts");'));
+
+/* Register the style sheet */
+
+function author_recent_posts_stylesheet() {
+            wp_register_style('author_stylesheet', plugins_url('css/authorPostsStyle.css', __FILE__) );
+            wp_enqueue_style('author_stylesheet');
+        }
+add_action('wp_enqueue_scripts', 'author_recent_posts_stylesheet');
+ 
+?>
---
+++ readme.txt
@@ -0,0 +1,62 @@
+=== Author Recent Posts ===
+Contributors: amitaits, kka284556
+Tags: author, authors, post, posts, recent, widget, thumbnail, thumbnails, sidebar, plugin, wordpress
+Requires at least: 3.0
+Tested up to: 3.8.1
+Stable tag: 1.0
+License: GPLv2 or later
+
+Author Recent Posts shows recent posts by an author on his post as a responsive sidebar Widget
+
+== Description ==
+
+This Plugin contains a responsive Widget for showing off recent posts by an author on all the single post pages created by that author. This Plugin is useful when you have multiple authors on your WordPress blog and you wish to display list of posts only created by particular author of the post.
+
+With this Plugin you can choose number of posts to display, show or hide featured image, dimensions of featured image, option to choose display post date or not. You can also show author name in title using '[author]' shortcode in Widget options. We will adding more options in the next version.
+
+== Installation ==
+
+This section describes how to install the Plugin and get it working.
+
+e.g.
+
+1. Install Author Recent Post Plugin either via the WordPress.org Plugin directory, or by uploading the author_recent_posts folder to `/wp-content/Plugins/` directory at your server
+2. Activate the Plugin through the 'Plugins' menu in WordPress
+3. Drag Author Recent Posts Widget from list of your `Available Widget` to the sidebar where you want to place the Widget
+4. You can change the setting as per your requirement or can opt default settings and you're ready
+
+
+== Frequently Asked Questions ==
+
+= How to use it? =
+Enable the Plugin and place the Widget in the sidebar wherever you want
+
+= Where does this Widget appear? =
+This Widget appears only to single posts on your WordPress blog
+
+= How can I show author name in Widget =
+Specify '[author]' shortcode (without quote) in title to display author name in Widget. Check screenshot for more detail
+
+= Is this responsive? =
+Yes, Widget created by Author Recent Posts Plugin is responsive
+
+= Does this Plugin has it's own CSS rule for coloring of links and texts? =
+No, this Plugin doesn't have any CSS rule for coloring of links and texts. It just inherit those from your theme's styles 
+
+== Screenshots ==
+1. Widget Options
+2. Viewing the Widget with thumbnail
+3. Viewing the Widget without thumbnail
+4. Specify '[author]' shortcode in Widget options
+5. Viewing author name in Widget
+
+== Changelog ==
+
+= 1.0 =
+* Let you specify a title with or without author name, set number of posts, enable/disable featured image and it's size, display or not post date
+
+
+== Upgrade Notice ==
+
+= 1.0 =
+Initial Release
